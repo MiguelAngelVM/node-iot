6d5e2128cb612218a3c98cb2a1da91154ab9d87b
MiguelAngelVM <mig3l_angel@hotmail.es>
2018-02-13 23:45:56 -0600

    implementacion de test utilizando ava

---
+++ iot-DB/tests/agent-tests.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const test = require('ava')
+let config = {
+    logging: function(){}
+}
+let db = null
+
+test.beforeEach(async()=>{
+    const setupDataBase = require('../')
+    db = await setupDataBase(config)
+})
+test('Agent', t =>{
+    t.truthy(db.Agent, 'Agent service should exist')
+})
